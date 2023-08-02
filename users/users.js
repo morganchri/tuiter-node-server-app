60eb2bdde5926a9ef50fc2b43b69e910636bd2e2
morganchri <morgan.chri@northeastern.edu>
2023-08-02 17:35:49 -0400

    first commit

---
+++ users/users.js
@@ -0,0 +1,5 @@
+export default [
+	{ "username": "alice",   "type": "FACULTY", "_id": "123", "tenured": false, "office": "None"},
+	{ "username": "bob",     "type": "STUDENT", "_id": "234", "tenured": false, "office": "None"},
+	{ "username": "charlie", "type": "FACULTY", "_id": "345", "tenured": false, "office": "None"}
+]
