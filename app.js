8311d642139e51b39448c985225ce932e4cfc198
morganchri <morgan.chri@northeastern.edu>
2023-08-03 11:42:41 -0400

    a5 REST su23

--- app.js
+++ app.js
@@ -12,12 +12,9 @@ app.use(express.json());
 app.use(
 	cors({
 			 credentials: true,
-			 origin: process.env.FRONTEND_URL,
+			 origin: process.env.FRONTEND_URL
 		 })
 );
-
-console.log("Frontend URL")
-console.log(process.env.FRONTEND_URL)
 
 const sessionOptions = {
 	secret: "any string",
--- users/auth-controller.js
+++ users/auth-controller.js
@@ -3,46 +3,39 @@ import * as usersDao from "./users-dao.js";
 
 const AuthController = (app) => {
 	const register = (req, res) => {
-		const register = (req, res) => {
-			const username = req.body.username;
-			const user = usersDao.findUserByUsername(username);
-			if (user) {
-				res.sendStatus(409);
-				return;
-			}
-			const newUser = usersDao.createUser(req.body);
-			req.session["currentUser"] = newUser;
-			res.json(newUser);
-		};
+		const username = req.body.username;
+		const user = usersDao.findUserByUsername(username);
+		if (user) {
+			res.sendStatus(409);
+			return;
+		}
+		const newUser = usersDao.createUser(req.body);
+		req.session["currentUser"] = newUser;
+		res.json(newUser);
 	};
 	const login    = (req, res) => {
-		const login = (req, res) => {
-			const username = req.body.username;
-			const password = req.body.password;
-			const user = usersDao.findUserByCredentials(username, password);
-			if (user) {
-				req.session["currentUser"] = user;
-				res.json(user);
-			} else {
-				res.sendStatus(404);
-			}
-		};
+		const username = req.body.username;
+		const password = req.body.password;
+		const user = usersDao.findUserByCredentials(username, password);
+		if (user) {
+			req.session["currentUser"] = user;
+			res.json(user);
+		} else {
+			res.sendStatus(404);
+		}
 	};
 	const profile  = (req, res) => {
-		const profile = (req, res) => {
-			const currentUser = req.session["currentUser"];
-			if (!currentUser) {
-				res.sendStatus(404);
-				return;
-			}
-			res.json(currentUser);
-		};
+		const currentUser = req.session["currentUser"];
+		if (!currentUser) {
+			res.sendStatus(404);
+			return;
+		}
+		res.json(currentUser);
 	};
-	const logout   = (req, res) => {
-		const logout = async (req, res) => {
-			req.session.destroy();
-			res.sendStatus(200);
-		};
+
+	const logout = async (req, res) => {
+		req.session.destroy();
+		res.sendStatus(200);
 	};
 	const update   = (req, res) => { };
 	app.post("/api/users/register", register);
