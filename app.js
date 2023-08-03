e60e95150411939fa29feb40472439d0c9cfb281
morganchri <morgan.chri@northeastern.edu>
2023-08-03 15:25:11 -0400

    a5 REST su23

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(
 	cors({
 			 credentials: true,
-			 origin: process.env.FRONTEND_URL
+			 // origin: process.env.FRONTEND_URL
+			 origin: ["http://localhost:3000", "https://a5--resilient-clafoutis-a7cc91.netlify.app"]
 		 })
 );
 
