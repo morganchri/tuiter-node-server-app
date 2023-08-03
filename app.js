e2bbd963834eb3f74c7b7fd0e8f77143ee926574
morganchri <morgan.chri@northeastern.edu>
2023-08-03 11:49:56 -0400

    a5 REST su23

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ import AuthController from "./users/auth-controller.js";
 
 const app = express()
 app.use(express.json());
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS);
+});
+
 app.use(
 	cors({
 			 credentials: true,
@@ -16,9 +21,7 @@ app.use(
 		 })
 );
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS);
-});
+
 
 const sessionOptions = {
 	secret: "any string",
