ca6961d62f351021dd703937fca1606cd008348e
morganchri <morgan.chri@northeastern.edu>
2023-08-03 20:38:16 -0400

    a5 REST su23

--- users/users.js
+++ users/users.js
@@ -1,5 +1,5 @@
 export default [
-	{ "username": "alice",   "type": "FACULTY", "_id": "123", "tenured": false, "office": "None"},
-	{ "username": "bob",     "type": "STUDENT", "_id": "234", "tenured": false, "office": "None"},
-	{ "username": "charlie", "type": "FACULTY", "_id": "345", "tenured": false, "office": "None"}
+	{ "username": "alice", "password":123,   "type": "FACULTY", "_id": "123", "tenured": false, "office": "None"},
+	{ "username": "bob",   "password":234,  "type": "STUDENT", "_id": "234", "tenured": false, "office": "None"},
+	{ "username": "charlie", "password":345,"type": "FACULTY", "_id": "345", "tenured": false, "office": "None"}
 ]
