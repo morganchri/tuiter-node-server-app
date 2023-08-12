b62742f6fae78852a126452f8b68d6c32ed1d721
morganchri <morgan.chri@northeastern.edu>
2023-08-12 14:56:01 -0400

    a6 MongoDB su23

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,32 +2,38 @@ import * as usersDao from "../users/users-dao.js";
 import usersController from "./users-controller.js";
 import * as UsersController from "./users-controller.js";
 import {updateUser} from "../users/users-dao.js";
+// import {updateUser} from "../users/users-dao.js";
 
 const AuthController = (app) => {
 
-	const register = (req, res) => {
-		const username = req.body.username;
-		const user = usersDao.findUserByUsername(username);
+	const register = async (req, res) => {
+		const user = await usersDao.findUserByUsername(req.body.username);
 		if (user) {
-			res.sendStatus(409);
+			res.sendStatus(403);
 			return;
 		}
-		const newUser = usersDao.createUser(req.body);
+		const newUser = await usersDao.createUser(req.body);
 		req.session["currentUser"] = newUser;
 		res.json(newUser);
 	};
 
-	const login    = (req, res) => {
+
+	const login = async (req, res) => {
 		const username = req.body.username;
 		const password = req.body.password;
-		const user = usersDao.findUserByCredentials(username, password);
-		if (user) {
-			req.session["currentUser"] = user;
-			res.json(user);
+		if (username && password) {
+			const user = await usersDao.findUserByCredentials(username, password);
+			if (user) {
+				req.session["currentUser"] = user;
+				res.json(user);
+			} else {
+				res.sendStatus(403);
+			}
 		} else {
-			res.sendStatus(404);
+			res.sendStatus(403);
 		}
 	};
+
 
 	const profile  = (req, res) => {
 		const currentUser = req.session["currentUser"];
@@ -43,12 +49,18 @@ const AuthController = (app) => {
 		res.sendStatus(200);
 	};
 
-	const update = (req, res) => {};
+	const update = async (req, res) => {
+
+		const id = req.params.uid;
+		const user = await usersDao.updateUser(id, req.body);
+		req.session["currentUser"] = user;
+		res.json(user);
+	};
 
 	app.post("/api/users/register",register);
 	app.post("/api/users/login",      login);
 	app.post("/api/users/profile",  profile);
 	app.post("/api/users/logout",    logout);
-	app.put ("/api/users",      updateUser);
+	app.put ("/api/users",      update);
 };
 export default AuthController;
--- users/users-dao.js
+++ users/users-dao.js
@@ -1,42 +1,18 @@
-import users from "./users.js"
+import usersModel from "./users-model.js";
 
-export const findAllUsers = () => users;
-
-export const findUserById = (uid) => {
-	const index = users.findIndex((u) => u._id === uid);
-	if (index !== -1) return users[index];
-	return null;
-};
+export const findAllUsers = () => usersModel.find();
 
 
-export const findUserByUsername = (username) => {
-	const index = users.findIndex((u) => u.username === username);
-	if (index !== -1) return users[index];
-	return null;
-};
-
-export const findUserByCredentials = (username, password) => {
-	const index = users.findIndex((u) => u.username === username && u.password === password);
-	if (index !== -1) {
-		return users[index];
-	}
-	return null;
-};
+export const findUserById = (id) => usersModel.findById(id);
 
 
-export const createUser = (user) => {
-	users.push(user);
-	return user;
-}
+export const findUserByUsername = (username) => usersModel.findOne({ username });
 
-export const updateUser = (uid, user) => {
-	const index = users.findIndex((u) => u._id === uid);
-	users[index] = { ...users[index], ...user };
-	return {status: 'ok'}
-};
+export const findUserByCredentials = (username, password) => usersModel.findOne({ username, password });
 
-export const deleteUser = (uid) => {
-	const index = users.findIndex((u) => u._id === uid);
-	users.splice(index, 1);
-	return {status: 'ok'}
-};
+
+export const createUser = (user) => usersModel.create(user);
+
+export const updateUser = (uid, user) => usersModel.updateOne({ _id: uid }, { $set: user });
+
+export const deleteUser = (uid) => usersModel.deleteOne({ _id: uid });
